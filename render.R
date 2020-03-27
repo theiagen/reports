f28fa451e39c29c12da6d90d62249417edb039ce
AbigailShockey <abbashockey@gmail.com>
2020-03-27 14:12:01 -0500

    Added render script

---
+++ render.R
@@ -0,0 +1,15 @@
+#!/usr/bin/env Rscript
+
+library(rmarkdown)
+library(tools)
+
+# Get command line arguments
+args = commandArgs(trailingOnly=TRUE)
+
+# Read inputs
+snp_mat <- read.table(file_path_as_absolute(args[1]), header = T)
+nwk <- file_path_as_absolute(args[2])
+report <- file_path_as_absolute(args[3])
+
+# Render the report
+render(report, output_file='monroe_report.pdf')
