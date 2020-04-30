36644a88e42206ebf3a494b70be2694ba34bfb3c
Abigail Shockey <abbashockey@gmail.com>
2020-04-30 16:41:51 -0500

    Update render_dryad.R

--- render_dryad.R
+++ render_dryad.R
@@ -9,8 +9,11 @@ args = commandArgs(trailingOnly=TRUE)
 # Read inputs
 snp_mat <- read.table(file_path_as_absolute(args[1]), header = T)
 nwk <- file_path_as_absolute(args[2])
-ar_df <- read.table(file_path_as_absolute(args[3]), header = T, stringsAsFactors = F)
-report <- file_path_as_absolute(args[4])
+report <- file_path_as_absolute(args[3])
+
+if (args[4]) {
+    ar_df <- read.table(file_path_as_absolute(args[4]), header = T, stringsAsFactors = F)
+}
 
 # Render the report
 render(report, output_file='report.pdf')
