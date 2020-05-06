d61bb6ee9bc3949de44cc1cb46994384ac8b2772
Abigail Shockey <abbashockey@gmail.com>
2020-05-06 09:41:20 -0500

    Update render_dryad.R

--- render_dryad.R
+++ render_dryad.R
@@ -5,13 +5,13 @@ library(tools)
 
 # Get command line arguments
 args = commandArgs(trailingOnly=TRUE)
+args_len <- length(args);
 
 # Read inputs
 snp_mat <- read.table(file_path_as_absolute(args[1]), header = T)
 nwk <- file_path_as_absolute(args[2])
 report <- file_path_as_absolute(args[3])
-
-if (args[4]) {
+if (args_len == 4) {
     ar_df <- read.table(file_path_as_absolute(args[4]), header = T, stringsAsFactors = F)
 }
 
