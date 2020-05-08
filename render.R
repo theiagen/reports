6d02dbca71a54170451941e9eb14702133037b4d
Abigail Shockey <abbashockey@gmail.com>
2020-05-08 16:00:30 -0500

    Update render.R

--- render.R
+++ render.R
@@ -8,13 +8,13 @@ args = commandArgs(trailingOnly=TRUE)
 args_len <- length(args);
 
 # Read inputs
-snp_mat <- read.table(file_path_as_absolute(args[1]), header = T)
+snp_mat <- read.table(file_path_as_absolute(args[1]), header = T, sep = "\t")
 nwk <- file_path_as_absolute(args[2])
 report <- file_path_as_absolute(args[3])
 
 # Check length of arguments
 if (args_len == 4) {
-    ar_df <- read.table(file_path_as_absolute(args[4]), header = T, stringsAsFactors = F)
+    ar_df <- read.table(file_path_as_absolute(args[4]), header = T, stringsAsFactors = F, sep = "\t")
 }
 
 # Render the report
