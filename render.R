90ec4a0f046c866a9a6e4d1cf374576328fd8772
Abigail Shockey <abbashockey@gmail.com>
2020-06-26 15:36:10 -0500

    Update reading snp matrix

--- render.R
+++ render.R
@@ -8,7 +8,7 @@ args = commandArgs(trailingOnly=TRUE)
 args_len <- length(args);
 
 # Read inputs
-snp_mat <- read.table(file_path_as_absolute(args[1]), header = T, check.names = FALSE)
+snp_mat <- read.table(file_path_as_absolute(args[1]), header = T, check.names = FALSE, sep = "\t", row.names = 1)
 nwk <- file_path_as_absolute(args[2])
 report <- file_path_as_absolute(args[3])
 
